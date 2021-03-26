5ba199a09975bf46fe0f35fc0d15bbb564aa6f9a
insssomniac <insssomniac@gmail.com>
2021-03-26 19:49:19 +0200

    balloon styles added

--- js/main.js
+++ js/main.js
@@ -2,26 +2,56 @@ ymaps.ready(init);
 function init(){
     myMap = new ymaps.Map("map", {
         center: [55.76, 37.64],
-        zoom: 15
+        zoom: 14
     }, {
-        balloonMaxWidth: 200,
+        balloonMaxWidth: 350,
+        balloonMaxHeight: 500,
         searchControlProvider: 'yandex#search'
     });
 
-    // Обработка события, возникающего при щелчке
-    // левой кнопкой мыши в любой точке карты.
-    // При возникновении такого события откроем балун.
     myMap.events.add('click', function (e) {
         if (!myMap.balloon.isOpen()) {
             var coords = e.get('coords');
             myMap.balloon.open(coords, {
-                contentHeader:'Событие!',
-                contentBody:'<p>Кто-то щелкнул по карте.</p>' +
-                    '<p>Координаты щелчка: ' + [
-                        coords[0].toPrecision(6),
-                        coords[1].toPrecision(6)
-                    ].join(', ') + '</p>',
-                contentFooter:'<sup>Щелкните еще раз</sup>'
+                contentBody:
+                    '<div class="balloon-content">' +
+                    '<div class="reviews">' +
+                    '  <div class="reviews__review review">' +
+                    '    <div class="review__header">' +
+                    '      <span class="review__name">Сергей</span>' +
+                    '      <span class="review__place-date">Кафе 12.01.2020</span>' +
+                    '    </div>' +
+                    '    <div class="review__text">Очень хорошее место!</div>' +
+                    '  </div>' +
+                    '  <div class="reviews__review review">' +
+                    '    <div class="review__header">' +
+                    '      <span class="review__name">Дима</span>' +
+                    '      <span class="review__place-date">Пятерочка 31.12.2020</span>' +
+                    '    </div>' +
+                    '    <div class="review__text">Вкусное шампанское!</div>' +
+                    '  </div>' +
+                    '  <div class="reviews__review review">' +
+                    '    <div class="review__header">' +
+                    '      <span class="review__name">Дима</span>' +
+                    '      <span class="review__place-date">Пятерочка 31.12.2020</span>' +
+                    '    </div>' +
+                    '    <div class="review__text">Вкусное шампанское!</div>' +
+                    '  </div>' +
+                    '</div>' +
+                    '<h1 class="balloon-title">Отзыв:</h1>' +
+                    '<form class="form" action="#">' +
+                    '  <label class="form__block">' +
+                    '    <input type="text" name="name" placeholder="Укажите ваше имя" class="form__input" required="">' +
+                    '  </label>' +
+                    '  <label class="form__block">' +
+                    '    <input type="text" name="place" placeholder="Укажите место" class="form__input" required="">' +
+                    '  </label>' +
+                    '  <label class="form__block">' +
+                    '    <textarea name="comment" class="form__input form__input--textarea" required="" placeholder="Оставить отзыв"></textarea>' +
+                    '  </label>' +
+                    '  <button type="submit" class="btn btn--orange">Добавить</button>' +
+                    '</form>' +
+                    '</div>'
             });
         }
         else {
@@ -29,16 +59,4 @@ function init(){
         }
     });
 
-    // Обработка события, возникающего при щелчке
-    // правой кнопки мыши в любой точке карты.
-    // При возникновении такого события покажем всплывающую подсказку
-    // в точке щелчка.
-    myMap.events.add('contextmenu', function (e) {
-        myMap.hint.open(e.get('coords'), 'Кто-то щелкнул правой кнопкой');
-    });
-
-    // Скрываем хинт при открытии балуна.
-    myMap.events.add('balloonopen', function (e) {
-        myMap.hint.close();
-    });
 }
