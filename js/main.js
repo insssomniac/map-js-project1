71dc2afa9bee9db855d63c13b4664df6b07bbe11
0xSheff <0xsheff@gmail.com>
2021-03-31 11:31:34 +0300

    added handlebars template and array of placemarks

--- js/main.js
+++ js/main.js
@@ -13,45 +13,8 @@ function init(){
         if (!myMap.balloon.isOpen()) {
             var coords = e.get('coords');
             myMap.balloon.open(coords, {
-                contentBody:
-                    '<div class="balloon-content">' +
-                    '<div class="reviews">' +
-                    '  <div class="reviews__review review">' +
-                    '    <div class="review__header">' +
-                    '      <span class="review__name">Сергей</span>' +
-                    '      <span class="review__place-date">Кафе 12.01.2020</span>' +
-                    '    </div>' +
-                    '    <div class="review__text">Очень хорошее место!</div>' +
-                    '  </div>' +
-                    '  <div class="reviews__review review">' +
-                    '    <div class="review__header">' +
-                    '      <span class="review__name">Дима</span>' +
-                    '      <span class="review__place-date">Пятерочка 31.12.2020</span>' +
-                    '    </div>' +
-                    '    <div class="review__text">Вкусное шампанское!</div>' +
-                    '  </div>' +
-                    '  <div class="reviews__review review">' +
-                    '    <div class="review__header">' +
-                    '      <span class="review__name">Дима</span>' +
-                    '      <span class="review__place-date">Пятерочка 31.12.2020</span>' +
-                    '    </div>' +
-                    '    <div class="review__text">Вкусное шампанское!</div>' +
-                    '  </div>' +
-                    '</div>' +
-                    '<h1 class="balloon-title">Отзыв:</h1>' +
-                    '<form class="form" action="#">' +
-                    '  <label class="form__block">' +
-                    '    <input type="text" name="name" placeholder="Укажите ваше имя" class="form__input" required="">' +
-                    '  </label>' +
-                    '  <label class="form__block">' +
-                    '    <input type="text" name="place" placeholder="Укажите место" class="form__input" required="">' +
-                    '  </label>' +
-                    '  <label class="form__block">' +
-                    '    <textarea name="comment" class="form__input form__input--textarea" required="" placeholder="Оставить отзыв"></textarea>' +
-                    '  </label>' +
-                    '  <button type="submit" class="btn btn--orange">Добавить</button>' +
-                    '</form>' +
-                    '</div>'
+                contentBody: balloonBody(reviews),
+                contentFooter: coords
             });
         }
         else {
@@ -59,4 +22,50 @@ function init(){
         }
     });
 
+    reviews.reviews.forEach((obj) => {
+        let placemark = new ymaps.Placemark(obj.coords);
+
+        myMap.geoObjects.add(placemark);
+    });
 }
+
+let reviews = {
+    "reviews": [
+        {
+            userName: "Сергей",
+            place: "Кафе",
+            review: "Очень хорошее место!",
+            date: "12.01.2020",
+            coords: [55.764331253399725, 37.622104301452616]
+        },
+        {
+            userName: "Дима",
+            place: "Пятерочка",
+            review: "Вкусное шампанское!",
+            date: "31.12.2020",
+            coords: [55.76316145474593, 37.62568992344673]
+        },
+        {
+            userName: "Виталий",
+            place: "ресторан",
+            review: "Хорошо внутри, вкусная еда",
+            date: "25.12.2020",
+            coords: [55.7627017218261, 37.61594814030466]
+        },
+        {
+            userName: "Елена",
+            place: "ресторан",
+            review: "Норм",
+            date: "25.12.2020",
+            coords: [55.76224198346906, 37.64040988652047]
+        }
+    ]
+}
+
+function balloonBody (data) {
+    const source = document.getElementById("balloon-template").innerHTML;
+    const template = Handlebars.compile(source);
+
+    return template(data);
+}
+
